33c551f8b2a77910c0501f98e0a91e242f453a41
Md Sofian Hasan <Dip.akand9899@gmail.com>
2025-05-07 16:50:31 +0600

    Backend, Express, Mongoose, dotenv, router

--- server/App/routes/web/enquiryRoutes.js
+++
@@ -1,7 +0,0 @@
-let express = require("express");
-const { enquiryInsert } = require("../../controllers/web/enquiryController");
-
-let enquiryRouter = express.Router();
-
-enquiryRouter.post("/insert", enquiryInsert);
-module.exports = enquiryRouter;
